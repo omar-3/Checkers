84e613f637175ca0b6f703ece6fcd11a4b82e5c6
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2021-03-09 02:54:20 +0200

    deleting target result

--- rustycheckers/target/rls/debug/deps/rustycheckers-3e938dc54819a8ff.d
+++ rustycheckers/target/rls/debug/deps/rustycheckers-3e938dc54819a8ff.d
@@ -1,6 +1,7 @@
-/Users/omarkhaled/Desktop/scribbles/rust/fundamentals/Checkers/rustycheckers/target/rls/debug/deps/rustycheckers-3e938dc54819a8ff.rmeta: src/lib.rs src/board.rs
+/Users/omarkhaled/Desktop/scribbles/rust/fundamentals/Checkers/rustycheckers/target/rls/debug/deps/rustycheckers-3e938dc54819a8ff.rmeta: src/lib.rs src/board.rs src/game.rs
 
-/Users/omarkhaled/Desktop/scribbles/rust/fundamentals/Checkers/rustycheckers/target/rls/debug/deps/rustycheckers-3e938dc54819a8ff.d: src/lib.rs src/board.rs
+/Users/omarkhaled/Desktop/scribbles/rust/fundamentals/Checkers/rustycheckers/target/rls/debug/deps/rustycheckers-3e938dc54819a8ff.d: src/lib.rs src/board.rs src/game.rs
 
 src/lib.rs:
 src/board.rs:
+src/game.rs:
